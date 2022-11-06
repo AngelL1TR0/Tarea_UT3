22248bc38ece2947e46187c76739ba1777c16093
Angel04 <angeltorija4@gmail.com>
2022-11-06 22:57:10 +0100

    fourth commit

--- src/main/java/service/ExcellService.java
+++ src/main/java/service/ExcellService.java
@@ -107,6 +107,7 @@ public class ExcellService {
     private List<OrdersToExcel> loadInfo() throws ParseException {
 
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar calendar = Calendar.getInstance();
 
         File OrderFile = new File(Configuration.PATH_XML);
         List<String> lines = fileDAO.readLineXML(OrderFile);
@@ -118,17 +119,23 @@ public class ExcellService {
             ordersList.add(new Orders(splitLine[0], splitLine[1], splitLine[2],splitLine[3], splitLine[4]));
             String saleDateString = splitLine[2];
             String deliverDateString = splitLine[3];
-            int sD= Integer.parseInt(splitLine[2]);
-            int dD= Integer.parseInt(splitLine[3]);
+            //int sD= Integer.parseInt(splitLine[2]);
+            //int dD= Integer.parseInt(splitLine[3]);
             Date deliverStatusDate = formato.parse(deliverDateString);
             Date saleStatusDate = formato.parse(saleDateString);
             String productStatus = "";
-            Calendar calendar = Calendar.getInstance();
+            Date today = calendar.getTime();
+            Date todayFormat = formato.parse(String.valueOf(today));
 
-            if (){
-
+            if (deliverStatusDate.before(todayFormat) && splitLine[4].equals("confirmado")){
+                productStatus = "en origen";
             }
-
+            if (deliverStatusDate.before(todayFormat) && splitLine[4].equals("pendiente")){
+                productStatus = "en recogida";
+            }
+            if (deliverStatusDate.before(todayFormat) && splitLine[4].equals("pendiente")) {
+                productStatus = "en recogida";
+            }
 
             ordersToExcels.add(new OrdersToExcel(splitLine[0],splitLine[1], saleStatusDate, deliverStatusDate, splitLine[4],productStatus ));
 
